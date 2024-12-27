df0781abea53fa2583e49f012aca50be280714f7
김희찬 <92291084+BetterTomorrowThanToday@users.noreply.github.com>
2024-12-27 13:54:29 +0900

    initial commit

---
+++ src/main/java/com/example/auttobackendadmin/AuttoBackendAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.auttobackendadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuttoBackendAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuttoBackendAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auttobackendadmin/AuttoBackendAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auttobackendadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuttoBackendAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
